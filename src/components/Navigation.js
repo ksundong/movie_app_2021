190359ae529385b1f4093fc91fd409888d851b8b
Dion <ksundong@gmail.com>
2021-08-29 12:53:24 +0900

    feat: 리액트 라우트에서 정보 보내는 방법 적용

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -5,7 +5,12 @@ function Navigation() {
   return (
     <div className="nav">
       <Link to="/">Home</Link>
-      <Link to="/about">About</Link>
+      <Link to={{
+        pathname: "/about",
+        state: {  // route에서 정보 보내는 방법
+          fromNavigation: true
+        }
+      }}>About</Link>
     </div>
   );
 }
--- src/routes/About.js
+++ src/routes/About.js
@@ -1,6 +1,7 @@
 import "./About.css";
 
-function About() {
+function About(props) {
+  console.log(props);
   return (
     <div className="about__container">
       <span>
